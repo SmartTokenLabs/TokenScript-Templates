a75629790c024401e07f0664b01aeb235729b883
nicktaras <nicktaras@hotmail.co.uk>
2024-05-30 08:59:51 +1000

    added ts for chain link tokens

---
+++ chainlink/src/routes/Bridge.svelte
@@ -0,0 +1,154 @@
+<script lang="ts">
+	import context from '../lib/context';
+	import Loader from '../components/Loader.svelte';
+	import Web3Logo from '../components/Web3Logo.svelte';
+	import CardArrow from '../components/CardArrow.svelte';
+	import MagnifyingGlass from '../components/MagnifyingGlass.svelte';
+	import Close from '../components/Close.svelte';
+	import type { ITokenContextData } from '@tokenscript/card-sdk/dist/types';
+
+	let token: ITokenContextData;
+	let loading = true;
+	let timeout: any;
+	let searchString: string;
+
+	const bridges = [
+		{
+			title: 'Arbitrum',
+			subTitle: 'Arbitrum One',
+			logo: 'Arbitrum',
+			url: 'https://bridge.arbitrum.io/'
+		},
+		{
+			title: 'Base',
+			subTitle: 'Base',
+			logo: 'Base',
+			url: 'https://bridge.base.org/'
+		},
+		{
+			title: 'Linea',
+			subTitle: 'Linea',
+			logo: 'Linea',
+			url: 'https://bridge.linea.build/'
+		},
+		{
+			title: 'Optimism',
+			subTitle: 'Optimism',
+			logo: 'Optimism',
+			url: 'https://app.optimism.io/bridge/deposit'
+		},
+		{
+			title: 'Polygon',
+			subTitle: 'Matic',
+			logo: 'Polygon',
+			url: 'https://portal.polygon.technology/bridge'
+		},
+		{
+			title: 'Klaytn',
+			subTitle: 'PortalBridge',
+			logo: 'PortalBridge',
+			url: 'https://portalbridge.com/advanced-tools/#/transfer?sourceChain=ethereum&targetChain=klaytn'
+		},
+		{
+			title: 'Scroll',
+			subTitle: 'Scroll',
+			logo: 'Scroll',
+			url: 'https://scroll.io/bridge'
+		},
+		{
+			title: 'zkSync',
+			subTitle: 'zkSync',
+			logo: 'ZkSync',
+			url: 'https://portal.zksync.io/bridge/'
+		}
+	];
+
+	let filteredBridges = bridges;
+
+	context.data.subscribe(async (value) => {
+		if (!value.token) return;
+		token = value.token;
+		loading = false;
+	});
+
+	function filterBridges(query: string) {
+		const lowercaseQuery = query.toLowerCase();
+		filteredBridges = bridges.filter((bridge) => {
+			const { title, subTitle } = bridge;
+			return (
+				title.toLowerCase().includes(lowercaseQuery) ||
+				subTitle.toLowerCase().includes(lowercaseQuery)
+			);
+		});
+	}
+</script>
+
+<div>
+	<div id="token-container" class="text-[#0c162d]">
+		<div class="field-section flex">
+			<div class="field-section-title text-3xl" style="font-size: 24px;">
+				Bridge ${token.symbol}
+			</div>
+			<img
+				style="width: 19px; margin: 0px 0px 0px 7px;"
+				alt="chainlink logo"
+				src="https://cdn.jsdelivr.net/gh/SmartTokenLabs/resources/images/logos/chainlink-link-logo.svg"
+			/>
+		</div>
+		<div class="field-section">
+			<div class="search-input flex items-center" style="margin-bottom: 12px">
+				<div class="ml-4">
+					<MagnifyingGlass />
+				</div>
+				<input
+					on:input={(event) => {
+						if (timeout) clearTimeout(timeout);
+						timeout = setTimeout(() => {
+							// @ts-ignore
+							searchString = event.target.value;
+							filterBridges(searchString);
+						}, 300);
+					}}
+					bind:value={searchString}
+					placeholder="Bridge or Network name"
+					id="search"
+					type="text"
+				/>
+				{#if searchString}
+					<button
+						class="w-4 h-4 border-0 bg-transparent absolute right-4"
+						on:click={(event) => {
+							event.stopPropagation();
+							searchString = '';
+							filterBridges(searchString);
+						}}
+					>
+						<Close />
+					</button>
+				{/if}
+			</div>
+			{#each filteredBridges as bridge}
+				<a
+					style="background: #0c162d; margin-bottom: 18px"
+					class="swap-card flex items-center justify-between border border-gray-700 rounded-lg p-6 text-[#3e4556] cursor-pointer transition duration-300 hover:border-gray-400 hover:text-[#3e4556]"
+					href={bridge.url}
+					target="_blank"
+				>
+					<div class="flex items-center">
+						<div style="padding-top: 9px">
+							<Web3Logo web3LogoRef={bridge.logo} />
+							<div style="margin-top: 9px;">
+								<div class="field-title text-white text-3xl">{bridge.title}</div>
+								<div class="field-title text-[#cecece] text-xl">{bridge.subTitle}</div>
+							</div>
+						</div>
+					</div>
+					<div class="flex justify-center items-center">
+						<CardArrow />
+					</div>
+				</a>
+			{/each}
+		</div>
+	</div>
+	<Loader show={loading} />
+</div>
---
+++ chainlink/src/routes/Info.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+	import context from '../lib/context';
+	import Loader from '../components/Loader.svelte';
+	import type { ITokenContextData } from '@tokenscript/card-sdk/dist/types';
+	import { formatWithByDecimalPlaces, chainConfig, formatTokenBalance } from '../lib/utils';
+	let token: ITokenContextData;
+	let tokenStats: any;
+	let loading = true;
+	let tokenStatsPriceEth: number | string = '-';
+	let tokenStatsPriceUsd: number | string = '-';
+	let userTokenAccountValueEth: number | string = '-';
+	let userTokenAccountValueUsd: number | string = '-';
+	let tokenBalance: number | string | undefined;
+
+	context.data.subscribe(async (value) => {
+		if (!value.token) return;
+		token = value.token;
+		tokenBalance = formatTokenBalance(token?._count, token?.decimals);
+		await setTokenPriceData();
+		loading = false;
+	});
+
+	async function setTokenPriceData() {
+		const tokenStatsRequest = await fetch(
+			`https://api.token-discovery.tokenscript.org/get-token-price?blockchain=evm&smartContract=0x514910771af9ca656af840dff83e8264ecf986ca&chain=eth`
+		);
+		tokenStats = await tokenStatsRequest.json();
+		if (tokenStats.value && tokenStats.usdPrice) {
+			tokenStatsPriceEth = tokenStats.value ?? 'not found';
+			tokenStatsPriceUsd = tokenStats?.usdPrice?.toLocaleString('en-US') ?? 'not found';
+
+			if (token._count) {
+				// @ts-ignore
+				userTokenAccountValueEth = Number(tokenStats.value) * Number(tokenBalance);
+				userTokenAccountValueUsd = Number(tokenStats.usdPrice) * Number(tokenBalance); // @ts-ignore
+			}
+		}
+	}
+</script>
+
+<div>
+	{#if token}
+		<div id="token-wrapper" class="text-[#0c162d]">
+			<div class="card-header-background">
+				<div class="text-center text-2xl pt-12 pb-6 text-[#385bd2] flex justify-center">
+					<img
+						style="width: 130px"
+						alt="chainlink logo"
+						src="https://cdn.jsdelivr.net/gh/SmartTokenLabs/resources/images/logos/Chainlink_Logo_Blue.svg"
+					/>
+				</div>
+				<div class="text-center">
+					<div class="text-lg label">Your Balance</div>
+					<div class="text-xl font-semibold field-value">
+						{tokenBalance
+							? formatWithByDecimalPlaces(Number(tokenBalance), 2) + '  $' + token.symbol
+							: '-'}
+					</div>
+				</div>
+				<div class="grid grid-cols-2 gap-4 my-12">
+					<div class="text-center">
+						<div class="text-sm label">Value ETH</div>
+						<div class="text-lg font-semibold">
+							{loading ? '-' : formatWithByDecimalPlaces(Number(userTokenAccountValueEth), 2)}
+						</div>
+					</div>
+					<div class="text-center">
+						<div class="text-sm label">Value USD</div>
+						<div class="text-lg font-semibold">
+							{loading ? '-' : formatWithByDecimalPlaces(Number(userTokenAccountValueUsd), 2)}
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="p-6" style="background-color: #0c162d">
+				<div class="flex items-center justify-between text-white">
+					<div class="text-lg font-semibold">Market Value</div>
+				</div>
+				<div class="mt-4 text-white">
+					<div class="text-sm text-[#a5baf5]">Ethereum</div>
+					<div class="text-lg font-semibold">
+						{tokenStatsPriceEth ? tokenStatsPriceEth.toString().substring(0, 6) + ' ETH' : '-'}
+					</div>
+				</div>
+				<div class="mt-4 text-white">
+					<div class="text-sm text-[#a5baf5]">USD</div>
+					<div class="text-lg font-semibold">
+						{tokenStatsPriceUsd
+							? '$' + formatWithByDecimalPlaces(Number(tokenStatsPriceUsd), 2)
+							: '-'}
+					</div>
+				</div>
+			</div>
+			<div class="p-6">
+				<div class="flex items-center justify-between">
+					<div class="text-lg font-semibold">Token</div>
+				</div>
+				<div class="mt-4">
+					<div class="text-sm label">Name</div>
+					<div class="text-lg font-semibold">{token.name ?? '-'}</div>
+				</div>
+				<div class="mt-4">
+					<div class="text-sm label">Symbol</div>
+					<div class="text-lg font-semibold">{token.symbol ?? '-'}</div>
+				</div>
+				<div class="mt-4">
+					<div class="text-sm label">Type</div>
+					<div class="text-lg font-semibold">ERC-20</div>
+				</div>
+				<div class="mt-4">
+					<div class="text-sm label">Chain</div>
+					<div style="margin-left: 2px; margin-top: 1.5px;" class="text-lg font-semibold">
+						{chainConfig[token.chainId].name}
+					</div>
+				</div>
+			</div>
+		</div>
+	{/if}
+	<Loader show={loading} />
+</div>
